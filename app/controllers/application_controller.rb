b1877e135921f775a995a2af3437ed71c1bec5ce
Kevin Nielsen <knielsen24@gmail.com>
2022-07-07 09:38:49 -0700

    commit tree eedac68f9051fafed9be18223b8057b883452a37

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
-  include ActionController::Cookies
+	include ActionController::Cookies
+
+rescue_from ActiveRecord::RecordInvalid, with: :record_not_valid
+
+	private
+
+	def record_not_valid(exception)
+		render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+	end
 
 end
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,43 +6,43 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 
 function App() {
-  const [user, setUser] = useState(null);
+   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    // auto-login
-    fetch("/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
+   useEffect(() => {
+      // auto-login
+      fetch("/me").then((r) => {
+         if (r.ok) {
+            r.json().then((user) => setUser(user));
+         }
+      });
+   }, []);
 
-  return (
-    <>
-      <NavBar user={user} setUser={setUser} />
-      <main>
-        {user ? (
-          <Switch>
-            <Route path="/">
-              <Home user={user}/>
-            </Route>
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path="/signup">
-              <SignUp setUser={setUser} />
-            </Route>
-            <Route path="/login">
-              <Login setUser={setUser} />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        )}
-      </main>
-    </>
-  );
+   return (
+      <>
+         <NavBar user={user} setUser={setUser} />
+         <main>
+            {user ? (
+               <Switch>
+                  <Route path="/">
+                     <Home user={user} />
+                  </Route>
+               </Switch>
+            ) : (
+               <Switch>
+                  <Route path="/signup">
+                     <SignUp setUser={setUser} />
+                  </Route>
+                  <Route path="/login">
+                     <Login setUser={setUser} />
+                  </Route>
+                  <Route path="/">
+                     <Home />
+                  </Route>
+               </Switch>
+            )}
+         </main>
+      </>
+   );
 }
 
 export default App;
